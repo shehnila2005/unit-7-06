dd14a24281758dc15e33724edcf44a1d61d98e5b
Shehnila Azmat <shehnila.azmat@ocsbstudent.ca>
2021-02-23 21:23:49 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const myage = 16
+let answer = 0 
+
+document.getElementById('button').addEventListener('click', buttonOn)
+
+function buttonOn () {
+  while (myage != answer) {
+    answer = prompt('guess my age')
+    if (answer == myage) {
+      alert ('correct')
+
+    } else if (answer < myage) {
+     alert ('you guessed to little, please try again')
+     
+    } else if (answer > myage) {
+    alert ('you guessed to big, please try again')
+    } else {
+}
+}
+}
