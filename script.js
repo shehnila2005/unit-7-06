2f7dee69a7a951c41eedd26cead5979ae4550511
Shehnila Azmat <shehnila.azmat@ocsbstudent.ca>
2021-02-23 21:48:32 +0000

    changes

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
 const myage = 16
-let answer = 0 
+let answer = 0
 
-document.getElementById('button').addEventListener('click', buttonOn)
+document.getElementById('buttonOn').addEventListener('click', buttonOn)
 
 function buttonOn () {
-  while (myage != answer) {
+    while (myage != answer) {
     answer = prompt('guess my age')
     if (answer == myage) {
-      alert ('correct')
-
-    } else if (answer < myage) {
-     alert ('you guessed to little, please try again')
-     
-    } else if (answer > myage) {
-    alert ('you guessed to big, please try again')
+      alert('correct')
+      } else if (answer < myage) {
+     alert('you guessed to little, please try again')
+      } else if (answer > myage) {
+    alert('you guessed to big, please try again')
     } else {
-}
-}
-}
+      }
+    }
+  }
